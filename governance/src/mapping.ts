384d389ed8fe889823b15dd70d553ce091dea0ff
Lucidao Developer <dev@lucidao.com>
2022-01-25 12:15:07 +0100

    governance subgraphspec

--- governance/src/mapping.ts
+++ governance/src/mapping.ts
@@ -1,35 +1,156 @@
-import { BigInt } from "@graphprotocol/graph-ts"
+import { ProposalUserVote, ProposalStatus } from './../generated/schema';
+import { Address, BigDecimal, BigInt, Bytes, ethereum } from "@graphprotocol/graph-ts"
 import {
   LucidaoGovernor,
-  AdminChanged,
-  BeaconUpgraded,
-  Upgraded
+  ProposalCanceled,
+  ProposalCreated,
+  ProposalExecuted,
+  ProposalQueued,
+  QuorumNumeratorUpdated,
+  TimelockChange,
+  VoteCast
 } from "../generated/LucidaoGovernor/LucidaoGovernor"
-import { ExampleEntity } from "../generated/schema"
+import { ProposalEntity } from "../generated/schema"
 
-export function handleAdminChanged(event: AdminChanged): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+namespace ProposalState {
+  const Pending = "Pending";
+  const Active = "Active";
+  const Canceled = "Canceled";
+  const Defeated = "Defeated";
+  const Succeeded = "Succeeded";
+  const Queued = "Queued";
+  const Expired = "Expired";
+  const Executed = "Executed";
+}
+type ProposalState = string;
+
+function getOrCreateProposalEntity(proposalId: BigInt): ProposalEntity {
+  let proposalEntity = ProposalEntity.load(proposalId.toHex())
 
   // Entities only exist after they have been saved to the store;
   // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  if (!proposalEntity) {
+    proposalEntity = new ProposalEntity(proposalId.toHex())
+  }
+  return proposalEntity;
+}
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+function getOrCreateVote(event: VoteCast): ProposalUserVote {
+  let voteId = event.transaction.hash.toHex() + "-" + event.logIndex.toString();
+  let voteEntity = ProposalUserVote.load(voteId)
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!voteEntity) {
+    voteEntity = new ProposalUserVote(voteId)
   }
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+  voteEntity.weight = event.params.weight;
+  voteEntity.support = event.params.support;
+  voteEntity.voter = event.params.voter;
+  voteEntity.reason = event.params.reason;
+  voteEntity.block = event.block.number;
+  voteEntity.blockTimestamp = event.block.timestamp;
 
-  // Entity fields can be set based on event parameters
-  entity.previousAdmin = event.params.previousAdmin
-  entity.newAdmin = event.params.newAdmin
+  return voteEntity;
+}
 
-  // Entities can be written to the store with `.save()`
-  entity.save()
+function setProposalStatus(proposalId: BigInt, proposalEntity: ProposalEntity, status: string, eventBlock: ethereum.Block, eventAddress: Address): void {
+  let statusId = proposalEntity.id + "-" + eventBlock.number.toString();
+  let statusEntity = ProposalStatus.load(statusId);
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!statusEntity) {
+    statusEntity = new ProposalStatus(statusId);
+  }
+
+  statusEntity.status = status;
+  statusEntity.statusBlockNumber = eventBlock.number;
+  statusEntity.statusBlockTimestamp = eventBlock.timestamp;
+  statusEntity.proposal = proposalEntity.id;
+  statusEntity.save();
+  // let contract = LucidaoGovernor.bind(eventAddress);
+  // chainStatus = contract.state(proposalId);
+}
+
+function setManualProposalStatus(proposalId: BigInt, proposalEntity: ProposalEntity, status: string, eventBlock: ethereum.Block, eventAddress: Address, blockDelay: BigInt): void {
+  let estimatedBlockNumber: BigInt = eventBlock.number.plus(blockDelay);
+  let mintingBlockEstimationInSeconds = BigDecimal.fromString("0.86")
+  let timestampDelta: BigInt = BigInt.fromString(blockDelay.toBigDecimal().times(mintingBlockEstimationInSeconds).truncate(0).toString());
+  let estimatedTimestamp: BigInt = eventBlock.timestamp.plus(timestampDelta);
+  let statusId = proposalEntity.id + "-" + estimatedBlockNumber.toString();
+  let statusEntity = ProposalStatus.load(statusId);
+
+  // Entities only exist after they have been saved to the store;
+  // `null` checks allow to create entities on demand
+  if (!statusEntity) {
+    statusEntity = new ProposalStatus(statusId);
+  }
+
+  statusEntity.status = status;
+  statusEntity.statusBlockNumber = estimatedBlockNumber;
+  statusEntity.statusBlockTimestamp = estimatedTimestamp;
+  statusEntity.proposal = proposalEntity.id;
+  statusEntity.save();
+}
+
+function slugify(myText: string): string {
+  const fromChar: string[] = "àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìıİłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;#".split("");
+  const toChar: string[] = "aaaaaaaaaacccddeeeeeeeegghiiiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz----   ".split("");
+
+  let slug = myText.toLowerCase();
+  for (var _i = 0; _i < fromChar.length; _i++) {
+    slug = slug.replaceAll(fromChar[_i], toChar[_i]);
+  }
+  return slug.split(" ").map<string>(y => y.trim()).filter(x => x.length > 0).join("_");
+}
+
+function createWebId(blockNumber: BigInt, proposalDescription: string): string {
+  //SPEC: <discourse id>-<will be ignored>-<Breve descrizione>
+  //Esempio1:  15 - [FLP #1] - Hello World
+  //Esempio2: 15-FLP-Vision: Become immortal
+  const proposalDescriptionChunks = proposalDescription.split("-");
+  if(proposalDescriptionChunks.length==1){
+    const slug = slugify(proposalDescription);
+    return `${blockNumber.toString()}-${slug}`;
+  }
+  const proposalDiscourseID = proposalDescriptionChunks[0];
+  const slug = slugify(proposalDescriptionChunks[proposalDescriptionChunks.length - 1]);
+  return `${proposalDiscourseID}-${slug}`;
+}
+
+function manageArrayParameters(proposalEntity: ProposalEntity, targets: ethereum.Value, callDatas: Bytes[], signatures: string[], values: BigInt[], parameters: ethereum.EventParam[]): ProposalEntity {
+  if (!proposalEntity.signatures) {
+    proposalEntity.signatures = [] as string[];
+  }
+
+  proposalEntity.signatures = signatures;
+  //proposalEntity.targets = event.params.targets; //https://github.com/graphprotocol/graph-ts/issues/246
+  let parsedTargets = [] as string[];
+  let targetsValues = targets.toArray();
+  for (var _i = 0; _i < targetsValues.length; _i++) {
+    parsedTargets.push(targetsValues[_i].toAddress().toHex().toString());
+  }
+
+  proposalEntity.targets = parsedTargets;
+  proposalEntity.values = values;
+
+  // proposalEntity.parametersName = [] as string[];
+  // proposalEntity.parametersValue = [] as string[];
+  // for (var _j = 0; _j < parameters.length; _j++) {
+  //   const ev = parameters[_j];
+  //   proposalEntity.parametersName.push(ev.name);
+  //   proposalEntity.parametersValue.push(ev.value.toString());
+  // }
+  proposalEntity.callDatas = callDatas;
+  return proposalEntity;
+}
+
+export function handleProposalCanceled(event: ProposalCanceled): void {
+  let proposalEntity = getOrCreateProposalEntity(event.params.proposalId);
+  proposalEntity.save();
+  setProposalStatus(event.params.proposalId, proposalEntity, ProposalState.Canceled, event.block, event.address);
 
   // Note: If a handler doesn't require existing field values, it is faster
   // _not_ to load the entity from the store. Instead, create it fresh with
@@ -46,10 +167,76 @@ export function handleAdminChanged(event: AdminChanged): void {
   // The following functions can then be called on this contract to access
   // state variables and other data:
   //
-  // - contract.admin(...)
-  // - contract.implementation(...)
+  // - contract.BALLOT_TYPEHASH(...)
+  // - contract.COUNTING_MODE(...)
+  // - contract.castVote(...)
+  // - contract.castVoteBySig(...)
+  // - contract.castVoteWithReason(...)
+  // - contract.getVotes(...)
+  // - contract.hasVoted(...)
+  // - contract.hashProposal(...)
+  // - contract.name(...)
+  // - contract.proposalDeadline(...)
+  // - contract.proposalEta(...)
+  // - contract.proposalSnapshot(...)
+  // - contract.proposalThreshold(...)
+  // - contract.proposalVotes(...)
+  // - contract.propose(...)
+  // - contract.queue(...)
+  // - contract.quorum(...)
+  // - contract.quorumDenominator(...)
+  // - contract.quorumNumerator(...)
+  // - contract.state(...)
+  // - contract.supportsInterface(...)
+  // - contract.timelock(...)
+  // - contract.token(...)
+  // - contract.version(...)
+  // - contract.votingDelay(...)
+  // - contract.votingPeriod(...)
 }
 
-export function handleBeaconUpgraded(event: BeaconUpgraded): void {}
+export function handleProposalCreated(event: ProposalCreated): void {
+  let proposalEntity = getOrCreateProposalEntity(event.params.proposalId);
+  proposalEntity.webId = createWebId(event.params.startBlock, event.params.description);
+  proposalEntity.description = event.params.description;
+  proposalEntity.startBlock = event.params.startBlock;
+  proposalEntity.endBlock = event.params.endBlock;
+  proposalEntity.transaction = event.transaction.hash.toHex();
+  proposalEntity.proposer = event.params.proposer;
 
-export function handleUpgraded(event: Upgraded): void {}
+  proposalEntity = manageArrayParameters(proposalEntity, event.parameters[2].value, event.params.calldatas, event.params.signatures, event.params.values, event.parameters);
+  proposalEntity.save()
+  setProposalStatus(event.params.proposalId, proposalEntity, ProposalState.Pending, event.block, event.address);
+  let contract = LucidaoGovernor.bind(event.address)
+  let votingPeriod: BigInt = contract.votingPeriod();
+  let votingDelay: BigInt = contract.votingDelay();
+  let votingExpiration: BigInt = votingPeriod.plus(votingDelay);
+
+  setManualProposalStatus(event.params.proposalId, proposalEntity, ProposalState.Active, event.block, event.address, votingDelay);
+  setManualProposalStatus(event.params.proposalId, proposalEntity, ProposalState.Expired, event.block, event.address, votingExpiration);
+}
+
+export function handleProposalExecuted(event: ProposalExecuted): void {
+  let proposalEntity = getOrCreateProposalEntity(event.params.proposalId);
+  proposalEntity.save()
+  setProposalStatus(event.params.proposalId, proposalEntity, ProposalState.Executed, event.block, event.address);
+}
+
+export function handleProposalQueued(event: ProposalQueued): void {
+  let proposalEntity = getOrCreateProposalEntity(event.params.proposalId);
+  proposalEntity.save()
+  setProposalStatus(event.params.proposalId, proposalEntity, ProposalState.Queued, event.block, event.address);
+}
+
+export function handleQuorumNumeratorUpdated(
+  event: QuorumNumeratorUpdated
+): void { }
+
+export function handleTimelockChange(event: TimelockChange): void { }
+
+export function handleVoteCast(event: VoteCast): void {
+  let proposalEntity = getOrCreateProposalEntity(event.params.proposalId);
+  let vote = getOrCreateVote(event);
+  vote.proposal = proposalEntity.id;
+  vote.save();
+}
